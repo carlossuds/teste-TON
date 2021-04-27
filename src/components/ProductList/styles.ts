e37e2d0cea49cc43e8360c49d3314f67cdb1c583
Carlos Sudsilowsky <sudsilowsky@gmail.com>
2021-04-26 23:10:11 -0300

    feat: MainPage and components Button, List, Item

---
+++ src/components/ProductList/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+import { colors } from '../../styles/colors';
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    paddingVertical: '5%',
+    paddingHorizontal: '5%',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+});
+
+export default styles;
